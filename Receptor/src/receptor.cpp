65ea1159ede3b2c6d7cbbe5676b0e259166592fd
Bianca Lima <biancalimasoares193@gmail.com>
2025-03-04 09:02:47 -0300

    Update RECEPTOR

--- Receptor/src/receptor.cpp
+++ Receptor/src/receptor.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <SPI.h>
 
 #define LORA_SCK 5
 #define LORA_MISO 19
@@ -10,16 +11,19 @@
 #define LORA_RST 14
 #define LORA_DI0 26
 
-// Configura√ß√µes WiFi
+#define SPREAD_FACTOR 12
+#define CODING_RATE 5
+
+// ConfiguraÁıes WiFi
 const char* ssid = "Nome da sua rede Wifi";
 const char* password = "Senha da sua rede Wifi";
 
-// Configura√ß√µes ThingSpeak
+// ConfiguraÁıes ThingSpeak
 const String apiKey = "Sua API do the thingSpeak";
 const String server = "http://api.thingspeak.com/update";
 
-void processReceivedData(const String &data);
-void sendToThingSpeak(float temp, float umid, int co2, bool chama, int risco);
+void processReceivedData(const String &data, int rssi, float snr);
+void sendToThingSpeak(float temp, float umid, int co2, bool chama, int risco, int rssi, float snr);
 
 void setup() {
   Serial.begin(115200);
@@ -30,7 +34,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nConectado √† WiFi!");
+  Serial.println("\nConectado ‡ WiFi!");
 
   // Inicializar LoRa
   LoRa.setPins(LORA_SS, LORA_RST, LORA_DI0);
@@ -42,30 +46,53 @@ void setup() {
 }
 
 void loop() {
-// Verificar conex√£o WiFi
+// Verificar conex„o WiFi
   if (WiFi.status() != WL_CONNECTED) {
     WiFi.reconnect();
+    Serial.println("Reconectando ao WiFi...");
     delay(5000);
   }
 
-  // Verificar se h√° um pacote LoRa
+  // Verificar se h· um pacote LoRa
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
+    long receivedTimestamp = 0;
     String receivedData = "";
+    // LÍ o RSSI do ˙ltimo pacote recebido
+    //int rssi = LoRa.packetRssi();
+    Serial.println("\n====================================================");
+    //Serial.print("RSSI: ");
+    //Serial.println(rssi);
     while (LoRa.available()) {
       receivedData += (char)LoRa.read();
     }
 
+    receivedTimestamp = receivedData.toInt();
+    long currentTime = millis();  // Captura o tempo atual ao receber o pacote
+    long uplinkTime = currentTime - receivedTimestamp;  // Calcula o tempo de uplink
+
     // Exibir dados recebidos
     Serial.println("Dados recebidos:");
     Serial.println(receivedData);
+    
+    int rssi = LoRa.packetRssi();
+    float snr = LoRa.packetSnr();
 
+    Serial.print(" | Tempo de Uplink: ");
+    Serial.print(uplinkTime);
+    Serial.print(" ms | RSSI: ");
+    Serial.print(rssi);
+    Serial.print(" dBm | SNR: ");
+    Serial.print(snr);
+    Serial.println(" dB");
+    
     // Processar os dados recebidos
-    processReceivedData(receivedData);
+    processReceivedData(receivedData, rssi, snr);
+
   }
 }
 
-void sendToThingSpeak(float temp, float umid, int co2, bool chama, int risco) {
+void sendToThingSpeak(float temp, float umid, int co2, bool chama, int risco, int rssi, float snr) {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     
@@ -74,27 +101,29 @@ void sendToThingSpeak(float temp, float umid, int co2, bool chama, int risco) {
              "&field2=" + String(umid) +
              "&field3=" + String(co2) +
              "&field4=" + String(chama) +
-             "&field5=" + String(risco);
+             "&field5=" + String(risco) +
+             "&field6=" + String(rssi) +
+             "&field7=" + String(snr);
 
 
     http.begin(url);
     int httpCode = http.GET();
 
 
-    // Verificar o c√≥digo HTTP e resposta do servidor
+    // Verificar o cÛdigo HTTP e resposta do servidor
     if (httpCode > 0) {
       String payload = http.getString();
-      Serial.printf("Dados enviados! C√≥digo HTTP: %d\nResposta: %s\n", httpCode, payload.c_str());
+      Serial.printf("Dados enviados! CÛdigo HTTP: %d\nResposta: %s\n", httpCode, payload.c_str());
     } else {
       Serial.printf("Erro no envio: %s\n", http.errorToString(httpCode).c_str());
     }
     http.end();
   } else {
-    Serial.println("WiFi desconectado, n√£o foi poss√≠vel enviar os dados.");
+    Serial.println("WiFi desconectado, n„o foi possÌvel enviar os dados.");
   }
 }
 
-void processReceivedData(const String &data) {
+void processReceivedData(const String &data, int rssi, float snr) {
   int tempIndex = data.indexOf("Temperatura: ");
   int co2Index = data.indexOf("CO2: ");
   int chamaIndex = data.indexOf("Chama: ");
@@ -114,9 +143,10 @@ void processReceivedData(const String &data) {
     Serial.println("CO2: " + co2 + " ppm");
     Serial.println("Chama: " + chama);
     Serial.println("Umidade: " + umidade + " %");
-    Serial.println("N√≠vel de Risco: " + risco);
-    sendToThingSpeak(temperatura.toFloat(), umidade.toFloat(), co2.toInt(), chama.toInt(), risco.toInt());
+    Serial.println("NÌvel de Risco: " + risco);
+    sendToThingSpeak(temperatura.toFloat(), umidade.toFloat(), co2.toInt(), chama.toInt(), risco.toInt(), rssi, snr);
   } else {
     Serial.println("Formato inesperado dos dados recebidos.");
   }
 }
+
